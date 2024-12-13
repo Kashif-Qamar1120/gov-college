25d141af1f2c325cbe69941400173d6b482cfb5f
Kashif Qamar <145191836+Kashif-Qamar1120@users.noreply.github.com>
2024-12-13 14:31:40 +0500

    Add files via upload

---
+++ college/gvt college/courses.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Template Mo">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+
+    <title>Education - List of Meetings</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-edu-meeting.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+    <link rel="stylesheet" href="assets/css/lightbox.css">
+<!--
+
+TemplateMo 569 Edu Meeting
+
+https://templatemo.com/tm-569-edu-meeting
+
+-->
+  </head>
+
+<body>
+
+<?php 
+ include './include/header.php'
+ ?>
+
+ 
+
+ 
+
+  <section class="meetings-page" id="meetings">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="filters">
+                <ul>
+                  <li data-filter="*"  class="active">All Courses</li>
+                  <li data-filter=".soon">Mining</li>
+                  <li data-filter=".imp">Electrical</li>
+                  <li data-filter=".att">Civil</li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-lg-12">
+              <div class="row grid">
+                <div class="col-lg-4 templatemo-item-col all soon">
+                  <div class="meeting-item">
+                    <div class="thumb">
+                     
+                      <a href="meeting-details.php"><img src="assets/images/dit.jpg" alt=""></a>
+                    </div>
+                    <div class="down-content">
+                      
+                      <a href="meeting-details.php"><h4>Diploma in Information Technology (DIT)</h4></a>
+                      <p>The DIT program provides practical knowledge in computer systems, programming, and network management, equipping students with essential IT skills for the digital age.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 templatemo-item-col all imp">
+                  <div class="meeting-item">
+                    <div class="thumb">
+                      
+                      <a href="meeting-details.php"><img src="assets/images/elect.jpg" alt=""></a>
+                    </div>
+                    <div class="down-content">
+                      
+                      <a href="meeting-details.php"><h4>Electrical</h4></a>
+                      <p>The Electrical Engineering diploma offers students comprehensive training in electrical systems, circuits, and power generation, preparing them for careers in the energy and automation sectors.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 templatemo-item-col all soon">
+                  <div class="meeting-item">
+                    <div class="thumb">
+                      
+                      <a href="meeting-details.php"><img src="assets/images/mining.jpg" alt=""></a>
+                    </div>
+                    <div class="down-content">
+                      
+                      <a href="meeting-details.php"><h4>Mining</h4></a>
+                      <p>The Mining Engineering course focuses on the extraction of natural resources, teaching students about mining processes, safety measures, and environmental impact, paving the way for careers in the mining industry.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 templatemo-item-col all att">
+                  <div class="meeting-item">
+                    <div class="thumb">
+                     
+                      <a href="meeting-details.php"><img src="assets/images/telecom.jpg" alt=""></a>
+                    </div>
+                    <div class="down-content">
+                      
+                      <a href="meeting-details.php"><h4>Telecommunication</h4></a>
+                      <p>The Telecommunication course at our institute provides students with in-depth knowledge of modern communication systems. It equips them with the skills needed to excel in the rapidly evolving field of telecommunication technology.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 templatemo-item-col all att">
+                  <div class="meeting-item">
+                    <div class="thumb">
+                     
+                      <a href="meeting-details.php"><img src="assets/images/mining.jpg" alt=""></a>
+                    </div>
+                    <div class="down-content">
+                      
+                      <a href="meeting-details.php"><h4>Mining</h4></a>
+                      <p>The Mining Engineering course focuses on the extraction of natural resources, teaching students about mining processes, safety measures, and environmental impact, paving the way for careers in the mining industry.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 templatemo-item-col all imp">
+                  <div class="meeting-item">
+                    <div class="thumb">
+                     
+                      <a href="meeting-details.php"><img src="assets/images/telecom.jpg" alt=""></a>
+                    </div>
+                    <div class="down-content">
+                     
+                      <a href="meeting-details.php"><h4>Telecommunication</h4></a>
+                      <p>The Telecommunication course at our institute provides students with in-depth knowledge of modern communication systems. It equips them with the skills needed to excel in the rapidly evolving field of telecommunication technology.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 templatemo-item-col all imp att">
+                  <div class="meeting-item">
+                    <div class="thumb">
+                      
+                      <a href="meeting-details.php"><img src="assets/images/elect.jpg" alt=""></a>
+                    </div>
+                    <div class="down-content">
+                      
+                      <a href="meeting-details.php"><h4>Electrical</h4></a>
+                      <p>The Electrical Engineering diploma offers students comprehensive training in electrical systems, circuits, and power generation, preparing them for careers in the energy and automation sectors.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 templatemo-item-col all soon imp">
+                  <div class="meeting-item">
+                    <div class="thumb">
+                      
+                      <a href="meeting-details.php"><img src="assets/images/dit.jpg" alt=""></a>
+                    </div>
+                    <div class="down-content">
+                     
+                      <a href="meeting-details.php"><h4>Diploma in Information Technology (DIT)</h4></a>
+                      <p>The DIT program provides practical knowledge in computer systems, programming, and network management, equipping students with essential IT skills for the digital age.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 templatemo-item-col all att soon">
+                  <div class="meeting-item">
+                    <div class="thumb">
+                      
+                      <a href="meeting-details.php"><img src="assets/images/mining.jpg" alt=""></a>
+                    </div>
+                    <div class="down-content">
+                      
+                      <a href="meeting-details.php"><h4>Mining</h4></a>
+                      <p>The Mining Engineering course focuses on the extraction of natural resources, teaching students about mining processes, safety measures, and environmental impact, paving the way for careers in the mining industry.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-12">
+              <div class="pagination">
+                <ul>
+                  <li><a href="#">1</a></li>
+                  <li class="active"><a href="#">2</a></li>
+                  <li><a href="#">3</a></li>
+                  <li><a href="#"><i class="fa fa-angle-right"></i></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <?php 
+   include './include/footer.php'
+   ?>
+
+   
+  </section>
+
+
+  <!-- Scripts -->
+  <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <script src="assets/js/isotope.min.js"></script>
+    <script src="assets/js/owl-carousel.js"></script>
+    <script src="assets/js/lightbox.js"></script>
+    <script src="assets/js/tabs.js"></script>
+    <script src="assets/js/isotope.js"></script>
+    <script src="assets/js/video.js"></script>
+    <script src="assets/js/slick-slider.js"></script>
+    <script src="assets/js/custom.js"></script>
+    <script>
+        //according to loftblog tut
+        $('.nav li:first').addClass('active');
+
+        var showSection = function showSection(section, isAnimate) {
+          var
+          direction = section.replace(/#/, ''),
+          reqSection = $('.section').filter('[data-section="' + direction + '"]'),
+          reqSectionPos = reqSection.offset().top - 0;
+
+          if (isAnimate) {
+            $('body, php').animate({
+              scrollTop: reqSectionPos },
+            800);
+          } else {
+            $('body, php').scrollTop(reqSectionPos);
+          }
+
+        };
+
+        var checkSection = function checkSection() {
+          $('.section').each(function () {
+            var
+            $this = $(this),
+            topEdge = $this.offset().top - 80,
+            bottomEdge = topEdge + $this.height(),
+            wScroll = $(window).scrollTop();
+            if (topEdge < wScroll && bottomEdge > wScroll) {
+              var
+              currentId = $this.data('section'),
+              reqLink = $('a').filter('[href*=\\#' + currentId + ']');
+              reqLink.closest('li').addClass('active').
+              siblings().removeClass('active');
+            }
+          });
+        };
+
+        $('.main-menu, .responsive-menu, .scroll-to-section').on('click', 'a', function (e) {
+          e.preventDefault();
+          showSection($(this).attr('href'), true);
+        });
+
+        $(window).scroll(function () {
+          checkSection();
+        });
+    </script>
+</body>
+
+
+  </body>
+
+</html>
