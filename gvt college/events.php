5017a401ce0f46b43884eb99a928cee9320388e8
Kashif Qamar <145191836+Kashif-Qamar1120@users.noreply.github.com>
2025-01-01 12:32:10 +0500

    srfs

--- gvt college/events.php
+++ gvt college/events.php
@@ -22,6 +22,8 @@
     <link rel="stylesheet" href="assets/css/owl.css">
     <link rel="stylesheet" href="assets/css/lightbox.css">
     <link rel="stylesheet" href="/vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="eventstyle.css">
+
 <!--
 
 TemplateMo 569 Edu Meeting
@@ -29,6 +31,9 @@ TemplateMo 569 Edu Meeting
 https://templatemo.com/tm-569-edu-meeting
 
 -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+ 
   </head>
 
 <body>
@@ -36,6 +41,210 @@ https://templatemo.com/tm-569-edu-meeting
  <?php 
  include './include/header.php'
  ?>
-<h2>Events page is under working</h2>
+
+<div class="section" style="margin-top: 3%;">
+        <div class="leftBox">
+          <div class="content">
+            <h1>Sagacity<sup>v16</sup> Events</h1>
+            <p><i>Sagacityⱽ¹⁶</i> is a two-day event, to be conducted in 20<sup>th</sup> & 21<sup>st</sup> January.<br>The events of <i>Sagacityⱽ¹⁶</i> is divided into two parts, <b>LAB EVENTS</b> and <b>NON-LAB EVENTS</b>.</p>
+          </div>
+        </div>
+        <div class="events">
+          <ul>
+            <li>
+              <div class="time">
+                <h2>Non-Lab<br><span>Event</span></h2>
+              </div>
+              <div class="details">
+                <h3>Pictionary (Solo)</h3>
+                <p>Pictionary is a drawing game where one person draws and other players try to guess what is being drawn.</p>
+                <a style="cursor: pointer;" class="revealNon">Participate</a>
+              </div>
+              <div style="clear: both;"></div>
+            </li>
+    
+            <li>
+              <div class="time">
+                <h2>Lab<br><span>Event</span></h2>
+              </div>
+              <div class="details">
+                <h3>CodeCombat</h3>
+                <p>Are you good at coding? Here is the best opputunity to win the race & be a Coder Of The Day!</p>
+                <a style="cursor: pointer;" class="revealNon">Participate</a>
+              </div>
+              <div style="clear: both;"></div>
+            </li>
+    
+            <li>
+              <div class="time">
+                <h2>Non-Lab<br><span>Event</span></h2>
+              </div>
+              <div class="details">
+                <h3>Sudoku (Solo)</h3>
+                <p>It is the most famous Japanese mathematical puzzle game.</p>
+                <a style="cursor: pointer;" class="revealNon">Participate</a>
+              </div>
+              <div style="clear: both;"></div>
+            </li>
+    
+            <li>
+              <div class="time">
+                <h2>Lab<br><span>Event</span></h2>
+              </div>
+              <div class="details">
+                <h3>TheLoBuilder</h3>
+                <p>Love to build Logos? Show the creativeness to make a logo on the given theme with the help of Photoshop or CorelDraw.</p>
+                <a style="cursor: pointer;" class="revealNon">Participate</a>
+              </div>
+              <div style="clear: both;"></div>
+            </li>
+    
+            <li>
+              <div class="time">
+                <h2>Non-Lab<br><span>Event</span></h2>
+              </div>
+              <div class="details">
+                <h3>Change the Look (Solo)</h3>
+                <p>Prove your artistic skills by making the given picture look alive. The most attractive person will be awarded.</p>
+                <a style="cursor: pointer;" class="revealNon">Participate</a>
+              </div>
+              <div style="clear: both;"></div>
+            </li>
+    
+            <li>
+              <div class="time">
+                <h2>Lab<br><span>Event</span></h2>
+              </div>
+              <div class="details">
+                <h3>Blind Typo Master</h3>
+                <p style="font-size: 12px;">Blind Typo Master is a typing competition to identify the fastest typer. The content to type will be available in English Script but having different language. Participants have to type while the screen will be switched off.</p>
+                <a style="cursor: pointer;" class="revealNon">Participate</a>
+              </div>
+              <div style="clear: both;"></div>
+            </li>
+            
+            <li>
+              <div class="time">
+                <h2>Non-Lab<br><span>Event</span></h2>
+              </div>
+              <div class="details">
+                <h3>Survey Says (Team of Two)</h3>
+                <p>In this game, 2 teams compete to name the most popular response to a survey in order to win the game.</p>
+                <a style="cursor: pointer;" class="revealNon">Participate</a>
+              </div>
+              <div style="clear: both;"></div>
+            </li>
+    
+            <li>
+              <div class="time">
+                <h2>Lab<br><span>Event</span></h2>
+              </div>
+              <div class="details">
+                <h3>Spin-a-web</h3>
+                <p>So, are you a web designer? Enter the thrilling world and make the best website using HTML, CSS, JavaScript and on the spot theme or concept.</p>
+                <a style="cursor: pointer;" class="revealNon">Participate</a>
+              </div>
+              <div style="clear: both;"></div>
+            </li>
+    
+            <li>
+              <div class="time">
+                <h2>Non-Lab<br><span>Event</span></h2>
+              </div>
+              <div class="details">
+                <h3>Name It! (Solo)</h3>
+                <p>The game which challenges participants to recognize various abbreviations, acronyms, symbols, apps & logos.</p>
+                <a style="cursor: pointer;" class="revealNon">Participate</a>
+              </div>
+              <div style="clear: both;"></div>
+            </li>
+    
+            <li>
+              <div class="time">
+                <h2>Lab<br><span>Event</span></h2>
+              </div>
+              <div class="details">
+                <h3>Debug_Thugs</h3>
+                <p style="font-size: 12px;">Detect the thugs out of the code, beat the bugs and get rid of them! Here, participants will be provided with C/C++ programs with errors. They have to find those errors and make code executable. *Compilers won't be provided!</p>
+                <a style="cursor: pointer;" class="revealNon">Participate</a>
+              </div>
+              <div style="clear: both;"></div>
+            </li>
+    
+            <li>
+              <div class="time">
+                <h2>Non-Lab<br><span>Event</span></h2>
+              </div>
+              <div class="details">
+                <h3>Funk from Junk (Team of two)</h3>
+                <p>Participants will be given 45 minutes to create a sellable product from waste.</p>
+                <a style="cursor: pointer;" class="revealNon">Participate</a>
+              </div>
+              <div style="clear: both;"></div>
+            </li>
+    
+            <li>
+              <div class="time">
+                <h2>Lab<br><span>Event</span></h2>
+              </div>
+              <div class="details">
+                <h3>TechTalash</h3>
+                <p style="font-size: 12px;">Are you good at guessing? Let's have fun predicting the Programming concepts. TechTalash is a quiz competition having indirected riddles where the participants have to guess the correct technical word.</p>
+                <a style="cursor: pointer;" class="revealNon">Participate</a>
+              </div>
+              <div style="clear: both;"></div>
+            </li>
+    
+            <li>
+              <div class="time">
+                <h2>Non-Lab<br><span>Event</span></h2>
+              </div>
+              <div class="details">
+                <h3>Wordsworth (Solo)</h3>
+                <p>In this game, the players will be given a word and they have to make small words out of it. One with the most wins.</p>
+                <a style="cursor: pointer;" class="revealNon">Participate</a>
+              </div>
+              <div style="clear: both;"></div>
+            </li>
+    
+            <li>
+              <div class="time">
+                <h2>Lab<br><span>Event</span></h2>
+              </div>
+              <div class="details">
+                <h3>Logical Masters</h3>
+                <p style="font-size: 10px;">Let's select * from brain & crack the code! Logical Masters will have two levels. In 1st level, participants have to solve SQL queries as fast as possible as this will be an elimination round. In the next round, participants will be provided by half-program & output. The one who completes the program & reaches the desired given output will win as the master!</p>
+                <a style="cursor: pointer;" class="revealNon">Participate</a>
+              </div>
+              <div style="clear: both;"></div>
+            </li>
+    
+            <li>
+              <div class="time">
+                <h2>Non-Lab<br><span>Event</span></h2>
+              </div>
+              <div class="details">
+                <h3>Debate (Team of Three)</h3>
+                <p style="font-size: 13px;">A debate is a discussion about a subject on which people have different view. Each team will support the topics given to them and counter-support the opposite teams' topic.</p>
+                <a style="cursor: pointer;" class="revealNon">Participate</a>
+              </div>
+              <div style="clear: both;"></div>
+            </li>
+    
+            <li>
+              <div class="time">
+                <h2>Lab<br><span>Event</span></h2>
+              </div>
+              <div class="details">
+                <h3>PicsMania</h3>
+                <p>Love editing? Using Photoshop, edit the given picture and make it more attractive.</p>
+                <a style="cursor: pointer;" class="revealNon">Participate</a>
+              </div>
+              <div style="clear: both;"></div>
+            </li>
+          </ul>
+        </div>
+    </div>
+
 </body>
 </html>
